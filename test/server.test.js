b8043ae0c9dc02537caf2c86976edc425ccecc02
Alastair McFarlane <alastair@alastairmcfarlane.co.uk>
2014-09-25 11:46:09 +0000

    Correct chai config syntax

--- test/server.test.js
+++ test/server.test.js
@@ -2,7 +2,7 @@ describe('Sanity server', function () {
   'use strict';
 
   var chai = require('chai');
-  chai.config.includeStack('true')
+  chai.config.includeStack(true)
   chai.should();
 
   var server = require('../server.js');
